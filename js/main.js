12c59cc119697f6afd1b380d5d2e4f2b629179ce
mich <843298603@qq.com>
2019-04-03 21:13:18 +0800

    第二次提交

--- js/main.js
+++ js/main.js
@@ -3,80 +3,77 @@ var $pics = $('#pics')
 var current = 0;
 var $images = $pics.children('img')
 
-var index = 1
+var index =1
 
 init()
 
-
-var timer = setInterval(function(){
-    if(index > $lis.length-1){
-        index = 0
+var timer = setInterval(() => {
+    if (index > $lis.length-1) {
+        index=0
     }
     goToNext(index)
     index++
-}, 3000)
+}, 1500);
 
-document.addEventListener('visibilitychange', function(e){
-    if(document.hidden){
+document.addEventListener('visibilitychange',function  (e) {
+    if (document.hidden) {
         window.clearInterval(timer)
-    }else{
-        timer = setInterval(function(){
-            if(index > $lis.length-1){
-                index = 0
+    } else {
+        timer = setInterval(() => {
+            if (index > $lis.length-1) {
+                index=0
             }
             goToNext(index)
             index++
-        }, 3000)
+        }, 1500);
     }
 })
 
 bindEvent()
 
-function bindEvent(){
-    $('.bar > ul').on('click', 'li', function(e){
+function bindEvent() {
+    $('.bar > ul').on('click','li',function  (e) {
         let $li = $(e.currentTarget)
         let index = $li.index()
         window.clearInterval(timer)
         goToNext(index)
-
-        timer = setInterval(function(){
-            if(index > $lis.length-1){
-                index = 0
-            }   
+        
+        timer = setInterval(() => {
+            if (index > $lis.length-1) {
+                index=0
+            }
             goToNext(index)
             index++
-        }, 3000)
-
+        }, 1500);
     })
-    
 }
 
-function goToNext(index){
-    if(index === 0 && current === $lis.length-1){ //last -> first
+function goToNext(index) {
+    if (index === 0 && current ===$lis.length-1) {//last -> first
         active(index)
-        $pics.css({transform: `translateX(-${($lis.length+1)*920}px)`}).one('transitionend', function(){
+        $pics.css({transform:`translateX(-${($lis.length+1)*920}px)`}).one('transitionend',function () {
             $pics.hide().offset()
-            $pics.css({transform: 'translateX(-920px)'}).show()
+            $pics.css({transform:`translateX(-920px)`}).show()
         })
-    }else if(index === $lis.length-1 && current === 0){ //first -> last
+    } else if(index === $lis.length-1 && current === 0){ //first -> last
         active(index)
-        $pics.css({transform: `translateX(0px)`}).one('transitionend', function(){
-            $pics.hide().offset()
-            $pics.css({transform: `translateX(-${(index+1)*920}px)`}).show()
+        $pics.css({transform:`translateX(0px)`}).one('transitionend',function () {
+            $pics.hide().offset
+            $pics.css({transform:`translateX(-${(index+1)*920}px)`}).show()
         })
-    }else{
-        $pics.css({transform: `translateX(-${(index+1)*920}px)`})
+    } else{
+        $pics.css({transform:`translateX(-${(index+1)*920}px)`})
         active(index)
     }
     current = index
 }
 
-function active(index){
+function active(index) {
     $lis.eq(index).addClass('active').siblings().removeClass('active')
 }
 
-function init(){
-    $pics.css({transform: 'translateX(-920px)'})
+function init() {
+    $pics.css({transform:`translateX(-920px)`})
     active(0)
     var $first = $images.eq(0).clone(true)
     var $last = $images.eq($images.length-1).clone(true)
